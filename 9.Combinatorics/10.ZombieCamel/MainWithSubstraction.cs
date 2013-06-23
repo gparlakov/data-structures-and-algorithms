761c8837b820b13d9878f38fe703f8f64c2b0638
gparlakov <gparlakov@gmail.com>
2013-06-23 12:51:13 -0700

    Combinatorics - Zombie Camel implemented with subtraction from larger sums

--- 9.Combinatorics/10.ZombieCamel/MainWithSubstraction.cs
+++ 9.Combinatorics/10.ZombieCamel/MainWithSubstraction.cs
@@ -1,71 +1,75 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace ZombieCamel
-//{
-//    public class ZombieCamelMain
-//    {
+namespace ZombieCamel
+{
+    public class ZombieCamelMain
+    {
+        static void Main()
+        {
+            var obeliscsDistances = ReadObeliscDistances();
+            long lengthOfAllObeliscs = Total(obeliscsDistances);
+            long totalLength = lengthOfAllObeliscs;
+            CalculatePermutations(obeliscsDistances, 0, lengthOfAllObeliscs, ref totalLength);
 
-//        static void Main()
-//        {
-//            var obeliscsDistances = ReadObeliscDistances();
-//            long lengthOfAllObeliscs = Total(obeliscsDistances);
-//            long totalLength = 0;
-//            CalculatePermutations(obeliscsDistances, new bool[obeliscsDistances.Length],
-//                lengthOfAllObeliscs, ref totalLength);
+            Console.WriteLine(totalLength);
+        }
 
-//            Console.WriteLine(totalLength);
-//        }
+        // We know the sum of all elements - on each level of recursion 
+        // we extract one of the elements from it
+        //thus recieving one combinations' sum
+        private static void CalculatePermutations(int[] elements, int indexOfMain,
+            long totalForThisLevel, ref long totalLenght)
+        {
 
-//        // We know the sum of all elements - on each level of recursion 
-//        // we extract one of the elements 
-//        // from it thus recieving one permutation's sum
-//        private static void CalculatePermutations(int[] elements, bool[] used,
-//            long totalForThisLevel, ref long totalLenght)
-//        {
-//            for (int i = 0; i < elements.Length; i++)
-//            {
-//                if (!used[i])
-//                {
-//                    used[i] = true;
-//                    long newTotal = totalForThisLevel - elements[i];
-//                    totalLenght += newTotal;
-//                    CalculatePermutations(elements, used, newTotal, ref totalLenght);
-//                    used[i] = false;
-//                }
-//            }
-//        }
+            for (int i = indexOfMain; i < elements.Length; i++)
+            {
+                long newTotal = totalForThisLevel - elements[i];
+                totalLenght += newTotal;
+                CalculatePermutations(elements, i + 1, newTotal, ref totalLenght);
+            }
+        }
 
-//        private static long Total(int[] obeliscsDistances)
-//        {
-//            var totalSum = 0L;
-//            for (int i = 0; i < obeliscsDistances.Length; i++)
-//            {
-//                totalSum += obeliscsDistances[i];
-//            }
+        private static long Total(int[] obeliscsDistances)
+        {
+            var totalSum = 0L;
+            for (int i = 0; i < obeliscsDistances.Length; i++)
+            {
+                totalSum += obeliscsDistances[i];
+            }
 
-//            return totalSum;
-//        }
+            return totalSum;
+        }
 
-//        private static int[] ReadObeliscDistances()
-//        {
-//            //var n = int.Parse(Console.ReadLine());
+        private static int[] ReadObeliscDistances()
+        {
+            var n = int.Parse(Console.ReadLine());
 
-//            //var obeliscDistances = new int[n];
-//            //for (int i = 0; i < n; i++)
-//            //{
-//            //    Console.ReadLine();
-//            //    var line = Console.ReadLine().Split(' ');
-//            //    var x = int.Parse(line[0].Trim());
-//            //    var y = int.Parse(line[1].Trim());
-//            //    obeliscDistances[i] = Math.Abs(x) + Math.Abs(y);
-//            //}
+            var obeliscDistances = new int[n];
+            var linesOfInput = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+                linesOfInput.AppendLine(Console.ReadLine());
+            }
 
-//            //return obeliscDistances;
+            var allInputNumbers = linesOfInput.ToString().Split(
+                new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-//            return new int[] { 1, 2, 3, 4 };
-//        }
-//    }
-//}
+            for (int i = 0; i < allInputNumbers.Length; i += 2)
+            {
+                var x = int.Parse(allInputNumbers[i].Trim());
+                var y = int.Parse(allInputNumbers[i + 1].Trim());
+
+                obeliscDistances[i / 2] = Math.Abs(x) + Math.Abs(y);
+            }
+            
+
+            return obeliscDistances;
+
+            //return new int[] { 1, 2, 3 };
+        }
+    }
+}
--- 9.Combinatorics/10.ZombieCamel/ZombieCamelMain.cs
+++ 9.Combinatorics/10.ZombieCamel/ZombieCamelMain.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace ZombieCamel
-{
-    public class ZombieCamelMain
-    {
-        static void Main()
-        {
-            var obeliscsDistances = ReadObeliscDistances();
-            long totalLength = 0;
-            for (int i = 1; i <= obeliscsDistances.Length; i++)
-            {
-                CalculatePermutations(obeliscsDistances, new int[i], 0, 0, ref totalLength);
-            }
+//namespace ZombieCamel
+//{
+//    public class ZombieCamelMain
+//    {
+//        static void Main()
+//        {
+//            var obeliscsDistances = ReadObeliscDistances();
+//            long totalLength = 0;
+//            for (int i = 1; i <= obeliscsDistances.Length; i++)
+//            {
+//                CalculatePermutations(obeliscsDistances, new int[i], 0, 0, ref totalLength);
+//            }
 
-            Console.WriteLine(totalLength);
-        }
+//            Console.WriteLine(totalLength);
+//        }
 
-        private static void CalculatePermutations(int[] obeliscsDistances, int[] permutation,
-            int permutationIndex, int elementsIndex, ref long totalLength)
-        {
-            if (permutationIndex == permutation.Length)
-            {
-                for (int i = 0; i < permutation.Length; i++)
-                {
-                    totalLength += permutation[i];
-                }
+//        private static void CalculatePermutations(int[] obeliscsDistances, int[] permutation,
+//            int permutationIndex, int elementsIndex, ref long totalLength)
+//        {
+//            if (permutationIndex == permutation.Length)
+//            {
+//                for (int i = 0; i < permutation.Length; i++)
+//                {
+//                    totalLength += permutation[i];
+//                }
 
-                return;
-            }
+//                return;
+//            }
 
-            for (int i = elementsIndex; i < obeliscsDistances.Length; i++)
-            {
-                permutation[permutationIndex] = obeliscsDistances[i];
-                CalculatePermutations(obeliscsDistances, permutation, permutationIndex + 1, i + 1, ref totalLength);
-            }
-        }
+//            for (int i = elementsIndex; i < obeliscsDistances.Length; i++)
+//            {
+//                permutation[permutationIndex] = obeliscsDistances[i];
+//                CalculatePermutations(obeliscsDistances, permutation, permutationIndex + 1, i + 1, ref totalLength);
+//            }
+//        }
 
-        private static int[] ReadObeliscDistances()
-        {
-            //var n = int.Parse(Console.ReadLine());
+//        private static int[] ReadObeliscDistances()
+//        {
+//            //var n = int.Parse(Console.ReadLine());
 
-            //var obeliscDistances = new int[n];
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.ReadLine();
-            //    var line = Console.ReadLine().Split(' ');
-            //    var x = int.Parse(line[0].Trim());
-            //    var y = int.Parse(line[1].Trim());
-            //    obeliscDistances[i] = Math.Abs(x) + Math.Abs(y);
-            //}
+//            //var obeliscDistances = new int[n];
+//            //for (int i = 0; i < n; i++)
+//            //{
+//            //    Console.ReadLine();
+//            //    var line = Console.ReadLine().Split(' ');
+//            //    var x = int.Parse(line[0].Trim());
+//            //    var y = int.Parse(line[1].Trim());
+//            //    obeliscDistances[i] = Math.Abs(x) + Math.Abs(y);
+//            //}
 
-            //return obeliscDistances;
+//            //return obeliscDistances;
 
-            return new int[] { 1, 2, 3, 4 };
-        }
-    }
-}
+//            return new int[] { 1, 2, 3 };
+//        }
+//    }
+//}
